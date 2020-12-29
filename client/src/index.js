53876662a3203afe71f05c759e0a9620d50ce79a
trieuquang <trieuquang071997@gmail.com>
2020-12-29 16:44:16 +0700

    Setup apollo

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import ApolloProvier from "./ApolloProvider";
+
+ReactDOM.render(ApolloProvier, document.getElementById("root"));
+
+reportWebVitals();
